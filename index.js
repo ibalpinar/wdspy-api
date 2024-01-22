70e12343dce498302224e460f3843d02847bacbd
ibalpinar <ilker@balpinar.com>
2024-01-23 00:32:21 +0300

    Add missing semicolon and add comment for user service

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-// index.js
-// Import the fastify framework
+/**
+ * /api/users GET: Returns all users in the datastore.
+ * /api/users/:userId GET: Returns a specific user.
+ * /api/users POST: Adds a new user.
+ * /api/users/:userId PUT: Edits a user.
+ * /api/users/:userId DELETE: Removes a user.
+*/
+
 const fastify = require('fastify');
 const app = fastify();
-// Set a GET route "/"
+
 app.get('/', function (request, reply) {
-    reply.send("Our first route");
+    reply.send("Our first route...");
 })
-// Start the server
+
 app.listen({port: 3000}, (err, address) =>  {
     if (err) {
         console.error(err);
         process.exit(1);
     }
     console.log(`Server listening on ${address}`);
-})
+});
