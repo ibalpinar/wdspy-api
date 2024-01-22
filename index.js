5f0a37fec093a737c60a39c4d944b1030ecc91e6
ibalpinar <ilker@balpinar.com>
2024-01-22 22:43:45 +0300

    Add semicolons to relevant lines

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // index.js
 // Import the fastify framework
-const fastify = require('fastify')
-const app = fastify()
+const fastify = require('fastify');
+const app = fastify();
 // Set a GET route "/"
 app.get('/', function (request, reply) {
-    reply.send("Our first route")
+    reply.send("Our first route");
 })
 // Start the server
 app.listen(3000, function (err, address) {
     if (err) {
-        console.error(err)
-        process.exit(1)
+        console.error(err);
+        process.exit(1);
     }
-    console.log(`Server listening on ${address}`)
+    console.log(`Server listening on ${address}`);
 })
